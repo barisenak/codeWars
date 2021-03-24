54168b4cd56b473e7764444154d2c8fd60eff4a8
maryia <maryia@wsb-015-72b.local>
2021-03-24 18:19:38 +0300

    24-03-21

--- 24-03-21.js
+++ 24-03-21.js
@@ -1,12 +1,13 @@
 //1 
 
-//You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. 
+//You will be given an array and a limit value. 
+//You must check that all values in the array are below or equal to the limit value. 
 //If they are, return true. Else, return false. You can assume all values in the array are numbers.
 
 
-function smallEnough(arr, limit){
+function smallEnough(a, limit){
     let answer = true
-    arr.forEach((item)=>{
+    a.forEach((item)=>{
       item > limit ? answer = false : true
     })
     return answer
