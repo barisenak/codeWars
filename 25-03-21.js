928c03440d3cfef4c0b17786422fc55bc8a31c00
maryia <maryia@wsb-015-72b.local>
2021-03-25 15:09:45 +0300

    modified

--- 25-03-21.js
+++ 25-03-21.js
@@ -7,31 +7,24 @@
 
 function duplicateEncode(word){
     let result = {}
-    let newWord = []
     let arr = word.toLowerCase().split("")
     arr.forEach((item)=>{
-      result[item] !== undefined ? ++result[item] : result[item] = 1
+      result[item] ? result[item]++ : result[item] = 1
     })
-    arr.map((item)=>{
-      result[item] == 1 ? newWord.push("(") : newWord.push(")")
-    })
-    return newWord.join("", "")
+    return arr.map((item)=>{
+      return result[item] == 1 ? "(" : ")"
+    }).join("")
 }
 
-
-
 //2
+
 //You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
 //The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
 //Write a method that takes the array as an argument and returns this "outlier" N.
 
 function findOutlier(arr){
-    let odd = 0
-    let even = 0
-    arr.forEach((item) =>{
-      item % 2 === 0 ? odd++ : even++
-    })
+    let odd = arr.filter(el => el % 2 === 0).length
     return arr.find((item) =>{
-     return odd < even ? item % 2 === 0 : item % 2 !== 0
+     return odd === 1 ? item % 2 === 0 : item % 2 !== 0
     })
   }
