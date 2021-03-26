1ca5967438f9d32ace23b03bd6668eb8e7715932
maryia <maryia@wsb-015-72b.local>
2021-03-26 17:18:12 +0300

    first commit

--- 26-03-21.js
+++ 26-03-21.js
@@ -8,4 +8,13 @@ function highAndLow(numbers){
     arr.length !== 1 ? arr.splice(1, arr.length-2) : arr.push(arr[0])
     return arr.join(" ")
       
-    }
+}
+
+//2
+
+//Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+//Return as a number.
+
+function divCon(arr){
+    return arr.reduce((acc, cur) => typeof cur === 'number'? acc + cur : acc - (+cur),0)
+  }
